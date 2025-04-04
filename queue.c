7bda5c4856a96c7ff6f666f4a310c26020c12684
PetrH14 <hortonp@purdue.edu>
2025-04-04 14:42:47 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -3,70 +3,111 @@
 
 // Function to enqueue a game state into the queue
 void enqueue(struct queue *q, struct game_state state) {
-    // Serialize the game state into an integer
     size_t serialized_state = serialize(state);
-
-    // Insert the serialized state at the tail of the linked list
     insert_at_tail(&q->data, serialized_state);
 }
 
 // Function to dequeue a game state from the queue
 struct game_state dequeue(struct queue *q) {
     if (q->data.head == NULL) {
-        // If the queue is empty, return an invalid game state (perhaps a sentinel value)
         struct game_state invalid_state;
-        invalid_state.board = NULL; // Set this to NULL or an invalid state representation
+        invalid_state.tiles[0][0] = 0;  // Mark as invalid state
         return invalid_state;
     }
 
-    // Remove the serialized state from the head of the list
     size_t serialized_state = remove_from_head(&q->data);
-
-    // Deserialize the integer back into a game state
-    struct game_state state = deserialize(serialized_state);
-
-    return state;
+    return deserialize(serialized_state);
 }
 
-// Function to calculate the number of moves needed to solve the puzzle (bfs or some heuristic)
-int number_of_moves(struct game_state start) {
-    // Assuming this function uses BFS (Breadth-First Search) to determine the shortest path
-    // to the solved state. You can implement BFS here, utilizing the queue for traversal.
+// Function to check if the puzzle is solved
+bool is_solved(struct game_state state) {
+    uint8_t solved[4][4] = {
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
+        { 13, 14, 15, 0 }
+    };
 
-    struct queue q;
-    q.data.head = NULL;  // Initialize an empty queue (linked list)
-    
-    // Enqueue the start state with 0 moves
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (state.tiles[i][j] != solved[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// Function to calculate the number of possible moves
+int num_possible_moves(struct game_state state) {
+    int possible_moves = 0;
+    if (state.empty_row > 0) possible_moves++;  // Can move up
+    if (state.empty_row < 3) possible_moves++;  // Can move down
+    if (state.empty_col > 0) possible_moves++;  // Can move left
+    if (state.empty_col < 3) possible_moves++;  // Can move right
+    return possible_moves;
+}
+
+// Function to generate the next game state based on a move
+struct game_state make_move(struct game_state state, int move) {
+    struct game_state next_state = state;
+
+    switch (move) {
+        case 0:
+            if (state.empty_row > 0) {
+                next_state.tiles[state.empty_row][state.empty_col] = state.tiles[state.empty_row - 1][state.empty_col];
+                next_state.tiles[state.empty_row - 1][state.empty_col] = 0;
+                next_state.empty_row--;
+                next_state.num_steps++;
+            }
+            break;
+        case 1:
+            if (state.empty_row < 3) {
+                next_state.tiles[state.empty_row][state.empty_col] = state.tiles[state.empty_row + 1][state.empty_col];
+                next_state.tiles[state.empty_row + 1][state.empty_col] = 0;
+                next_state.empty_row++;
+                next_state.num_steps++;
+            }
+            break;
+        case 2:
+            if (state.empty_col > 0) {
+                next_state.tiles[state.empty_row][state.empty_col] = state.tiles[state.empty_row][state.empty_col - 1];
+                next_state.tiles[state.empty_row][state.empty_col - 1] = 0;
+                next_state.empty_col--;
+                next_state.num_steps++;
+            }
+            break;
+        case 3:
+            if (state.empty_col < 3) {
+                next_state.tiles[state.empty_row][state.empty_col] = state.tiles[state.empty_row][state.empty_col + 1];
+                next_state.tiles[state.empty_row][state.empty_col + 1] = 0;
+                next_state.empty_col++;
+                next_state.num_steps++;
+            }
+            break;
+    }
+
+    return next_state;
+}
+
+// Function to calculate the number of moves needed to solve the puzzle
+int number_of_moves(struct game_state start) {
+    struct queue q = { .data.head = NULL };
+
     enqueue(&q, start);
 
-    // Create a set or list to track visited states (you can use a hash table or array)
-    // This part is pseudo-code to illustrate:
-    // initialize_visited_set();
-
     while (q.data.head != NULL) {
-        // Dequeue the current state
         struct game_state current_state = dequeue(&q);
 
-        // Check if the current state is the goal state (solved puzzle)
         if (is_solved(current_state)) {
-            return current_state.moves; // Return the number of moves when the goal is found
+            return current_state.num_steps;
         }
 
-        // Generate possible moves and enqueue them
-        // For each possible move, create a new game state
         for (int i = 0; i < num_possible_moves(current_state); i++) {
-            struct game_state next_state = make_move(current_state, i); // Generate next state
-
-            // If this state hasn't been visited, enqueue it
-            // if (!visited(next_state)) {
+            struct game_state next_state = make_move(current_state, i);
             enqueue(&q, next_state);
-            // }
-
-            // Update visited set (add next_state to the visited list)
-            // visited_add(next_state);
         }
     }
 
-    // If the goal state is not reachable (shouldn't happen in a solvable puzzle)
-    return -1;  // Return -1 indicating failure or unreachable state
+    return -1;  // If the goal state is not reachable
 }
